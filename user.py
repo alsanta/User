4c5e8ff045a89489beb97b46de9d107529ce1ce7
Alex Santana <alexandresntn@yahoo.com>
2021-07-07 00:22:11 -0500

    chained the methods

--- user.py
+++ user.py
@@ -2,19 +2,24 @@ class User:
     def __init__(self, name):
         self.account_balance = 0
         self.name = name
+
     def make_deposit(self, amount):
         self.account_balance += amount
         print (f'{self.name} deposits: {amount}\n Your balance is: {self.account_balance}')
+        return self
 
     def make_withdrawl(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
             print (f'{self.name} withdraws: {amount}\n Your balance is: {self.account_balance}')
+            return self
         else:
             print(f'You attempted to witdraw {amount} but only have a balance of {self.account_balance}\n The transaction was cancelled')
+            return self
 
     def disply_user_balance(self):
-        print(f'{self.name} balance is {self.account_balance}')
+        print(f"{self.name}'s balance is {self.account_balance}")
+        return self
 
     def transfer_money(self, amount, name):
         self.account_balance -= amount
@@ -25,19 +30,10 @@ alex = User('Alex')
 bob = User('Bob')
 joe = User('Joe')
 
-alex.make_deposit(500)
-alex.make_deposit(1500)
-alex.make_deposit(1000)
-alex.make_withdrawl(500)
+alex.make_deposit(500).make_deposit(1500).make_deposit(1000).make_withdrawl(500).disply_user_balance()
 
-bob.make_deposit(500)
-bob.make_deposit(500)
-bob.make_withdrawl(1000)
-bob.make_withdrawl(500)
+bob.make_deposit(500).make_deposit(500).make_withdrawl(1000).make_withdrawl(500).disply_user_balance()
 
-joe.make_deposit(5000)
-joe.make_withdrawl(1000)
-joe.make_withdrawl(1000)
-joe.make_withdrawl(500)
+joe.make_deposit(5000).make_withdrawl(1000).make_withdrawl(1000).make_withdrawl(500)
 
 alex.transfer_money(500,joe)
